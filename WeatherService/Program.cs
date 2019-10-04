00bb96c7555febba9b1c2be90826eb330a78d969
Sibusiso Labi <skylasbu@gmail.com>
2019-10-04 09:36:22 +0200

    Weather Service: Cape Town

---
+++ WeatherService/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using WeatherServiceLibrary.Models;
+using WeatherServiceLibrary;
+
+namespace WeatherService
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var displayWeather = await CTWeatherWebService.GetWeather();
+            Console.WriteLine("The Weather Report for Cape Town is");
+            Console.WriteLine(displayWeather.Temperature.ToString());
+
+
+            Console.WriteLine(displayWeather.Humidity.ToString());
+            Console.WriteLine(displayWeather.Weather);
+        }
+    }
+}
---
+++ WeatherServiceLibrary/CTWeatherWebService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using WeatherServiceLibrary.Models;
+    
+
+namespace WeatherServiceLibrary
+{
+    public class CTWeatherWebService
+    {
+        public async static Task<WeatherInformation> GetWeather()
+        {
+            HttpClient http = new HttpClient();
+            var ctWbService = await http.GetStringAsync("http://api.openweathermap.org/data/2.5/weather?q=Cape%20Town&APPID=f27a016865a3eed617a112811e03e4ba");
+
+            WeatherInfo weather = JsonConvert.DeserializeObject<WeatherInfo>(ctWbService);
+
+            /*   List<string> fullReport = new List<string>();
+
+               for (int index = 0; index < weather.weather.Count; index++)
+               {
+                   fullReport.Add(weather.weather[index].description);
+                   fullReport.Add(weather.weather[index].id.ToString());
+                   fullReport.Add(weather.weather[index].main);
+                   fullReport.Add(weather.name[index].ToString());
+                   fullReport.Add(weather.timezone.ToString());
+                   fullReport.Add(weather.coord.lat.ToString());
+
+               }
+               */
+            WeatherInformation info = new WeatherInformation();
+            info.Temperature = weather.main.temp;
+            info.Humidity = weather.main.humidity;
+
+
+
+           foreach (var n in weather.weather)
+            {
+                info.Weather = n.description;
+               
+            }
+
+
+            return info;
+
+            
+       
+            
+
+        }
+    }
+}
